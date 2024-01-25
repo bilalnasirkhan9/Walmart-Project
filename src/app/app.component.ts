ae16891cd1d1ad0e46983d0529ec453a1a206d8c
Bilal Nasir <bilal2308f@aptechorangi.com>
2024-01-24 17:31:42 -1200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+  <app-header></app-header>
   `,
   styles: [],
 })
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <p>
+      I din't Known but its working
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
